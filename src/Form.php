c004719c8d2874120b14818cf1eeb6ee1f262495
Lucas Hart <lucas.hart@aztecweb.net>
2019-11-26 09:39:51 -0300

    Input sendo impresso no WP através do twig

--- src/Form.php
+++ src/Form.php
@@ -48,26 +48,7 @@ class Form
     }
 
     public static function text()
-    {
-        $twig = new Twig();
-        $form = new AztecForm();
-
-        $form->add('task', EmailType::class, [
-            'required' => false,
-            'label' => 'my_label',
-            'data' => 'a123',
-            'attr' => [
-                'class' => 'teste',
-                'id' => 'my_id'
-            ]
-        ]);
-
-        $context = [
-            'form' => $form->createView(),
-        ];
-
-        $input =  $twig->render('row.html.twig', $context);
-        
+    {        
         $args = array(
             'field_id' => 'myprefix_setting-id-4',
             'field_title' => 'Field Title',
@@ -81,61 +62,38 @@ class Form
             'wrapper_class' => 'field-wrapper-class'
         );
         
-        add_action('admin_init', function() use ($args){
+        add_action( 'admin_init', function() use ( $args ){
             add_settings_field(
                 $args['field_id'],
                 $args['field_title'],
-                function() use ($args){
-                    echo '<input
-                        name="'. $args['field_id'] .'"
-                        type="email"
-                        id="'. $args['field_id'] .'"
-                        value="' . get_option($args['field_id']) . '"
-                        class="regular-text ltr"
-                        >';
+                function() use ( $args ){
+                    $twig = new Twig();
+                    $form = new AztecForm();
+
+                    $form->add( 'task', EmailType::class, [
+                        'required' => false,
+                        'label' => 'my_label',
+                        'data' => get_option( $args['field_id'] ),
+                        'compound' => true,
+                        'row_attr' => [
+                            'name' => 'myname',
+                            'id' => $args['field_id']
+                        ],
+                        'attr' => [
+                            'class' => 'teste',
+                        ]
+                    ] );
+            
+                    $context = [
+                        'form' => $form->createView(),
+                    ];
+            
+                    echo  $twig->render( 'row.html.twig', $context );
                 },
                 $args['group'],
                 'default',
                 $args
             );
         });
-
-        // /** ------------------------------------------------------ */
-        // $field_id = 'general';
-        // $group = 'myprefix_setting-id-4';
-        // $args = array(
-        //     'label' => "Field title",
-        //     'sanitize_callback' => 'sanitize_text_field',
-        //     'validation_callback' => 'some_validation_function',
-        //     'description' => 'Field description',
-        //     'default_value' => 'Default',
-        //     'wrapper_class' => 'field-wrapper-class'
-        // );
-        // add_action('admin_init', function () use ($field_id, $group, $args, $input) {
-        //     register_setting($group, $field_id, [
-        //         'type' => 'string',
-        //         'sanitize_callback' => $args['sanitize_callback'],
-        //         'description' => $args['description'],
-        //         'show_in_rest' => true,
-        //         'default' => $args['default_value'],
-        //     ]);
-
-        //     add_settings_field(
-        //         $field_id,
-        //         $args['label'],
-        //         $input,
-        //         $group,
-        //         'default',
-        //         array(
-        //             'field_id' => $field_id,
-        //             'label_for' => $field_id,
-        //             'wrapper_class' => $args['wrapper_class'],
-        //         )
-        //     );
-
-        //     if (isset($_POST)) {
-        //         ErrorNotice::render($field_id, 'Notice error render');
-        //     }
-        // });
     }
 }
